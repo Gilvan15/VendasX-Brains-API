ff5833a2a6a5f63f0ebf78b2347d6b19771ab0a9
Gilvan Jr <gilvanx10@gmail.com>
2022-05-04 19:27:02 -0300

    criação dos sub pacotes: entity, controllers, repository e service

--- vendas/src/test/java/com/apixbrain/vendas/VendasApplicationTests.java
+++ vendas/src/test/java/com/apixbrain/vendas/VendasApplicationTests.java
@@ -30,57 +30,21 @@ class VendasApplicationTests {
 
 	@Test
 	void contextLoads() throws ParseException {
-		
-		
-		String sDateInicio = "31/03/2022";
-		String sDateFinal = "31/04/2022";
-		SimpleDateFormat formatter1 = new SimpleDateFormat("dd/MM/yyyy");
 
-		Date date1 = formatter1.parse(sDateInicio);
-		Date date2 = formatter1.parse(sDateFinal);
+		/*
+		Anotações commitis:
 
-		List<ListarVendaTotalVendedores> listTemp = new ArrayList<>();
-
-		List<Venda> vendas = vendasRepository.findAll();
-		List<Vendedor> vendedor = vendedorRepository.findAll();
+			01 "commit inicial - estrutura basica do projeto vendaxbraintest com as dependencias: h2,web,jpa,lombok,devtools - mavem"
+			02 "criação do pacote raíz: domain"
+			03 "criação dos sub pacotes: entity, controllers, repository e service"
+		*/
 
 		
-		for (Vendedor vd : vendedor) {
-			
-			int contador = (int) vendas.stream().filter(vds -> (vds.getVendedor().getId() == vd.getId()))
-			.count();
-			
-			double somatotalVenda = vendas.stream().filter(vds -> (vds.getVendedor().getId() == vd.getId() && vds.getDataVenda().after(date1)
-							&& vds.getDataVenda().before(date2)))
-					.map(item -> item.getValor()).mapToDouble(Double::doubleValue).sum();
-
-			if (somatotalVenda != 0) {
-				vtotalmeida = totalmedias(somatotalVenda, contador);
-				listTemp.add(new ListarVendaTotalVendedores(vd.getId(), vd.getNome(), somatotalVenda, vtotalmeida, contador));
-			}
-
-		}
 		
-		for(ListarVendaTotalVendedores lt: listTemp) {
-			
-			System.out.println(lt.getId() + "-" + lt.getNome() + " - " + "- " 
-			+ lt.getTotalvendasvendedor() +" - " + lt.getMediavendasdiarias());
-			
-		}
 
 		
 	}
 
-	public Double totalmedias(Double media, int contador) {
-
-		totalmedia = (media / contador);
-		return arredondar(totalmedia);
-	}
-
-	private static double arredondar(double media) {
-		return Math.round(media * 100.0) / 100.0;
-
-	}	
 		
 }
 
